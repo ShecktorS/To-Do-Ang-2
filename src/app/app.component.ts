0b1f963314ee2f896b1608a40fdf89ead2ab2fc4
ShecktorS <ettoresanfilippo@gmail.com>
2023-07-30 12:03:09 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'to-do-Ang';
+export class AppComponent implements OnInit {
+  newTask: string = '';
+  taskEdited: string = '';
+  list = [
+    { title: 'pippo', edit: false, completed: false },
+    { title: 'lino', edit: false, completed: false },
+    { title: 'sdsd', edit: false, completed: false },
+  ];
+
+  ngOnInit(): void {}
+
+  // Per aggiungere un elemento
+  addTask = () => {
+    if (this.list.some((todo) => todo.title === this.newTask)) {
+      alert('La task che stai tentando di inserire esiste già!');
+    } else {
+      this.list.push({ title: this.newTask, edit: false, completed: false });
+      this.newTask = '';
+    }
+  };
+
+  // Per rimuovere un elemento
+  deleteTask = (i: number) => {
+    this.list.splice(i, 1);
+  };
+
+  changeEditStatus(i: number, edit: boolean) {
+    this.list[i].edit = edit;
+    this.taskEdited = this.list[i].title;
+  }
+
+  editTaskTitle(i: number) {
+    this.list[i].title = this.taskEdited;
+    this.list[i].edit = false;
+  }
 }
