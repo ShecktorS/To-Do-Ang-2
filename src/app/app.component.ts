d5e3415c29fdbca9e5ee8942236edf404c10235b
ShecktorS <ettoresanfilippo@gmail.com>
2023-07-31 11:57:09 +0200

    to-do upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,25 +9,42 @@ export class AppComponent implements OnInit {
   newTask: string = '';
   taskEdited: string = '';
   list = [
-    { title: 'pippo', edit: false, completed: false },
-    { title: 'lili', edit: false, completed: false },
+    { title: 'Prova', edit: false, completed: false },
+    { title: 'con LocalStorage', edit: false, completed: false },
   ];
+  storedList: [] = [];
 
-  ngOnInit(): void {}
+  rewriteSotrage = () => {
+    localStorage.setItem('list', JSON.stringify(this.list));
+  };
+
+  constructor() {}
+
+  ngOnInit() {
+    const data = JSON.parse(localStorage.getItem('list') || '');
+    this.storedList = data;
+    this.list = this.storedList;
+  }
+
+  ngOnDestroy() {}
 
   // Per aggiungere un elemento
   addTask = () => {
     if (this.list.some((todo) => todo.title === this.newTask)) {
       alert('La task che stai tentando di inserire esiste già!');
+    } else if (this.newTask.length < 1) {
+      alert('Non Puoi Inserire una task vuota');
     } else {
       this.list.push({ title: this.newTask, edit: false, completed: false });
       this.newTask = '';
     }
+    this.rewriteSotrage();
   };
 
   // Per rimuovere un elemento
   deleteTask = (i: number) => {
     this.list.splice(i, 1);
+    this.rewriteSotrage();
   };
 
   changeEditStatus(i: number, edit: boolean) {
@@ -38,9 +55,11 @@ export class AppComponent implements OnInit {
   editTaskTitle(i: number) {
     this.list[i].title = this.taskEdited;
     this.list[i].edit = false;
+    this.rewriteSotrage();
   }
 
   changeStatus(i: number) {
     this.list[i].completed = !this.list[i].completed;
+    this.rewriteSotrage();
   }
 }
